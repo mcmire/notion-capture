d47104ca1ccf0c85e2134d9c8f3a42521bff1c34
Elliot Winkler <elliot.winkler@gmail.com>
2021-05-22 23:45:43 -0600

    Use configuration when removing tmp files

--- lib/notion_capture/sidekiq.rb
+++ lib/notion_capture/sidekiq.rb
@@ -1,3 +1,4 @@
 require_relative '../notion_capture'
 
-FileUtils.rm_rf(NotionCapture::ROOT.join('tmp'))
+FileUtils.rm_rf(NotionCapture.configuration.local_repo_dir)
+FileUtils.rm_rf(NotionCapture.configuration.lockfile_path)
