973e7613fb77ee6c63914836cf43b4ba5b0358ff
Elliot Winkler <elliot.winkler@gmail.com>
2021-05-22 23:37:45 -0600

    Revert "Run dotenv in notion_capture.rb"
    This reverts commit 46ff085ab64d9621fe7e6049b9df80db93980449.

--- lib/notion_capture/git_repo_factory.rb
+++ lib/notion_capture/git_repo_factory.rb
@@ -45,8 +45,10 @@ module NotionCapture
         {
           credentials:
             Rugged::Credentials::UserPassword.new(
-              username: ENV['GITHUB_USERNAME'],
-              password: ENV['GITHUB_PASSWORD'],
+              {
+                username: ENV['GITHUB_USERNAME'],
+                password: ENV['GITHUB_PASSWORD'],
+              },
             ),
         }
       else
